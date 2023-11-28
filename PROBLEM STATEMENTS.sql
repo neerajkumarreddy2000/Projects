fd1749a6a5074444700d859d45c3e3a7392afe25
neerajkumarreddy2000 <151948702+neerajkumarreddy2000@users.noreply.github.com>
2023-11-28 17:35:40 +0530

    Add files via upload

---
+++ PROBLEM STATEMENTS.sql
@@ -0,0 +1,51 @@
+--KPI'S REQUIREMENT
+
+--1)TOTAL REVENUE:The Sum of total price of all pizza orders
+
+--2)TOTAL ORDERS:Total number of orders placed
+
+--3)AVERAGE ORDER VALUE: The Average amountspent per order,TOTAL REVENUE/TOTAL ORDERS
+
+--4)TOTAL PIZZAS SOLD: Sum of the quantities of all pizzas sold
+
+--5)AVERAGE PIZZAS SOLD PER ORDER: TOTAL PIZZAS SOLD/TOTAL ORDERS
+
+
+
+--CHARTS REQUIREMENT
+
+/* 1)Daily Trend for Total Orders:
+
+   Create a bar chart that displays the daily trend of total orders over a specific time period.  */
+
+/* 2)Monthly trend for total orders:
+
+    Create a bar chart that displays the monthly trend of total orders over a specific time period. */
+
+/* 3)Percentages of sales by pizza category:
+
+    Create a pie chart that shows the distribution of sales across different categories  */
+
+/* 4))Percentages of sales by pizza size:
+
+   Create a pie chart that shows the distribution of sales across different categories .Helps to understand customer preferences */
+
+/* 5) Total pizzas sold by pizza category:
+
+   Create a funnel chart .This chart helps to compare the sales performance of different categories */
+
+/* 6)Top 5 best sellers by Revenue,total quantity and total orders:
+
+   create a bar chart highlighting best selling pizzas based on revenue,total quantity and total orders.
+
+/* 7)Bottom 5 best sellers by Revenue,total quantity and total orders:
+
+   create a bar chart highlighting worst selling pizzas based on revenue,total quantity and total orders.
+   helps to identify underforming pizzas..*/
+
+
+   
+
+
+    
+
